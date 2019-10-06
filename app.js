c2fa69ba9ae0408acc2ed01e3862ececf94540c7
Terrell Douglas <terrelloydouglas@yahoo.com>
2019-10-05 17:27:10 -0700

    added JS

---
+++ app.js
@@ -0,0 +1,63 @@
+var seattle = {
+    location: 'Seattle',
+    minCust: 23,
+    maxCust: 65,
+    avgCookie: 6.3,
+
+    function getRandom(seattle.minCust , seattle.maxCust) {
+        return Math.random() * (seattle.maxCust - seattle.minCust) + seattle.minCust;
+    }
+
+}
+console.log(seattle);
+
+var tokyo = {
+    location: 'Tokyo',
+    minCust: 3,
+    maxCust: 24,
+    avgCookie: 1.2,
+
+    function getRandom(seattle.minCust , seattle.maxCust) {
+        return Math.random() * (seattle.maxCust - seattle.minCust) + seattle.minCust;
+    }
+
+}
+console.log(seattle);
+
+var dubai = {
+    location: 'Dubai',
+    minCust: 11,
+    maxCust: 38,
+    avgCookie: 3.7,
+
+    function getRandom(seattle.minCust , seattle.maxCust) {
+        return Math.random() * (seattle.maxCust - seattle.minCust) + seattle.minCust;
+    }
+
+}
+console.log(seattle);
+
+var paris = {
+    location: 'Paris',
+    minCust: 20,
+    maxCust: 38,
+    avgCookie: 2.3,
+
+    function getRandom(seattle.minCust , seattle.maxCust) {
+        return Math.random() * (seattle.maxCust - seattle.minCust) + seattle.minCust;
+    }
+
+}
+console.log(seattle);
+
+var lima = {
+    location: 'Lima',
+    minCust: 2,
+    maxCust: 16,
+    avgCookie: 4.6,
+
+    function getRandom(seattle.minCust , seattle.maxCust) {
+        return Math.random() * (seattle.maxCust - seattle.minCust) + seattle.minCust;
+    }
+
+}console.log(seattle);
