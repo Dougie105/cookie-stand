b95f7948ca1f304fe64b1dde738ad1a0f454fb70
Terrell Douglas <terrelloydouglas@yahoo.com>
2019-10-06 22:47:00 -0700

    added .eslint, gitignore, and reset.css. Also added tabels to the sales sheet linked to recieve a random number, just needs to have a total calculator

--- app.js
+++ app.js
@@ -1,84 +1,84 @@
 var seattle = {
-    location: 'Seattle',
-    minCust: 23,
-    maxCust: 65,
-    avgCookie: 6.3,
+  location: 'Seattle',
+  minCust: 23,
+  maxCust: 65,
+  avgCookie: 6.3,
+  totalSales: 0,
 
-    whatsThat: function(){
-        seattle.minCust = Math.ceil(seattle.minCust);
-        seattle.maxCust = Math.floor(seattle.maxCust);
-        return Math.floor(Math.random() * (seattle.maxCust - seattle.minCust)) + seattle.minCust;
-    } 
+  whatsThat: function(){
+    this.minCust = Math.ceil(this.minCust);
+    this.maxCust = Math.floor(this.maxCust);
+    return Math.floor(Math.random() * (this.maxCust - this.minCust)) + seattle.minCust;
+  }
 
-}
+};
 console.log(seattle.whatsThat());
 
 
-
 var tokyo = {
-    location: 'Tokyo',
-    minCust: 3,
-    maxCust: 24,
-    avgCookie: 1.2,
+  location: 'Tokyo',
+  minCust: 3,
+  maxCust: 24,
+  avgCookie: 1.2,
 
-    whatsThat: function(){
-        tokyo.minCust = Math.ceil(tokyo.minCust);
-        tokyo.maxCust = Math.floor(tokyo.maxCust);
-        return Math.floor(Math.random() * (tokyo.maxCust - tokyo.minCust)) + tokyo.minCust;
-    } 
+  whatsThat: function(){
+    tokyo.minCust = Math.ceil(tokyo.minCust);
+    tokyo.maxCust = Math.floor(tokyo.maxCust);
+    return Math.floor(Math.random() * (tokyo.maxCust - tokyo.minCust)) + tokyo.minCust;
+  }
 
-}
+};
 console.log(tokyo.whatsThat());
 
 
 
 var dubai = {
-    location: 'Dubai',
-    minCust: 11,
-    maxCust: 38,
-    avgCookie: 3.7,
+  location: 'Dubai',
+  minCust: 11,
+  maxCust: 38,
+  avgCookie: 3.7,
 
-    whatsThat: function(){
-        dubai.minCust = Math.ceil(dubai.minCust);
-        dubai.maxCust = Math.floor(dubai.maxCust);
-        return Math.floor(Math.random() * (dubai.maxCust - dubai.minCust)) + dubai.minCust;
-    } 
+  whatsThat: function(){
+    dubai.minCust = Math.ceil(dubai.minCust);
+    dubai.maxCust = Math.floor(dubai.maxCust);
+    return Math.floor(Math.random() * (dubai.maxCust - dubai.minCust)) + dubai.minCust;
+  }
 
-}
+};
 console.log(dubai.whatsThat());
 
 
 
 var paris = {
-    location: 'Paris',
-    minCust: 20,
-    maxCust: 38,
-    avgCookie: 2.3,
+  location: 'Paris',
+  minCust: 20,
+  maxCust: 38,
+  avgCookie: 2.3,
 
-    whatsThat: function(){
-        paris.minCust = Math.ceil(paris.minCust);
-        paris.maxCust = Math.floor(paris.maxCust);
-        return Math.floor(Math.random() * (paris.maxCust - paris.minCust)) + paris.minCust;
-    } 
+  whatsThat: function(){
+    paris.minCust = Math.ceil(paris.minCust);
+    paris.maxCust = Math.floor(paris.maxCust);
+    return Math.floor(Math.random() * (paris.maxCust - paris.minCust)) + paris.minCust;
+  }
 
-}
+};
 console.log(paris.whatsThat());
 
 
 
 var lima = {
-    location: 'Lima',
-    minCust: 2,
-    maxCust: 16,
-    avgCookie: 4.6,
+  location: 'Lima',
+  minCust: 2,
+  maxCust: 16,
+  avgCookie: 4.6,
 
-    whatsThat: function(){
-        lima.minCust = Math.ceil(lima.minCust);
-        lima.maxCust = Math.floor(lima.maxCust);
-        return Math.floor(Math.random() * (lima.maxCust - lima.minCust)) + lima.minCust;
-    } 
+  whatsThat: function(){
+    lima.minCust = Math.ceil(lima.minCust);
+    lima.maxCust = Math.floor(lima.maxCust);
+    return Math.floor(Math.random() * (lima.maxCust - lima.minCust)) + lima.minCust;
+  }
 
-}
+};
 console.log(lima.whatsThat());
 
 
